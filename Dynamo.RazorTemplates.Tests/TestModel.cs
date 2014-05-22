650981eb710fca10b6c9ed8379ff606bc746830f
MartinF <from@pc.dk>
2014-05-22 03:45:40 -0400

    Modified TestModel to include IEnumerable property

--- Dynamo.RazorTemplates.Tests/TestModel.cs
+++ Dynamo.RazorTemplates.Tests/TestModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Dynamo.RazorTemplates.Tests
 {
 	public class TestModel
 	{
 		public String String { get; set; }
+		public IEnumerable<TestModel> Models { get; set; }
 
 		public String GetString()
 		{
